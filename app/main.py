73afb01d74cbbf87a5c728b0136b21bde94513bd
thomasdunlap <tom.a.dunlap@gmail.com>
2021-08-23 11:37:40 -0700

    feature: Added query functionality to define data expectations for api inputs.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import joblib
 
 app = FastAPI()
@@ -6,10 +6,15 @@ model = joblib.load("app/models/sota_model.joblib")
 
 @app.get("/")
 def read_root():
-    return {"API status": "Tom is great!"}
+    return {"API status": "If you can see this, it's working!"}
 
 @app.post("/predict/")
-def predict_price(location, size, bedrooms):
+def predict_price(
+    location: str = Query(..., description="Location", min_length=1),
+    size: float = Query(..., description="Size (in meters squared)", ge=10.0),
+    bedrooms: int = Query(2, description="Number of bedrooms", ge=0, le=50)
+    ):
+    
     example = [location, size, bedrooms]
     price = model.predict([example])[0]
     return {"prediction": int(price)}
