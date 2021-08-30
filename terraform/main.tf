cb8af596d26cd33f7f4c9a44fee2ff02a48f9244
thomasdunlap <tom.a.dunlap@gmail.com>
2021-08-29 19:36:14 -0700

    feature:  Added terraform ECS Fargate deployment code.  Working api prediction endpoint.

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,19 +7,67 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-west-2"
+variable region {
+ default = "us-west-2"
 }
+
+provider "aws" {
+  region = var.region
+}
+
+data aws_caller_identity current {}
+ 
+locals {
+ prefix = "prediction"
+ account_id          = data.aws_caller_identity.current.account_id
+ ecr_repository_name = "${local.prefix}-api-repository"
+ ecr_image_tag       = "latest"
+}
+
 
 resource "aws_ecr_repository" "api_ecr" {
-  name = "prediction-api-repository"
+  name = local.ecr_repository_name
 }
 
+resource null_resource ecr_image {
+ triggers = {
+   python_file = md5(file(abspath("${path.module}/../app/main.py")))
+   docker_file = md5(file(abspath("${path.module}/../Dockerfile")))
+ }
+ 
+ provisioner "local-exec" {
+   command = <<EOF
+           aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${local.account_id}.dkr.ecr.${var.region}.amazonaws.com
+           cd ${path.module}/../
+           docker build -t prediction-api-repository .
+           docker tag prediction-api-repository:latest ${aws_ecr_repository.api_ecr.repository_url}:${local.ecr_image_tag}
+           docker push ${aws_ecr_repository.api_ecr.repository_url}:${local.ecr_image_tag}
+       EOF
+ }
+}
+
+data aws_ecr_image ecs_image {
+ depends_on = [
+   null_resource.ecr_image
+ ]
+ repository_name = local.ecr_repository_name
+ image_tag       = local.ecr_image_tag
+}
+
+
 data "aws_vpc" "default" {
+  depends_on = [
+   null_resource.ecr_image
+  ]
+
   default = true
 }
 
 data "aws_internet_gateway" "default" {
+  depends_on = [
+   null_resource.ecr_image
+  ]
+
   filter {
     name = "attachment.vpc-id"
     values = [data.aws_vpc.default.id]
@@ -27,6 +75,10 @@ data "aws_internet_gateway" "default" {
 }
 
 module "subnets" {
+  depends_on = [
+   null_resource.ecr_image
+  ]
+
   source              = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.32.0"
   namespace           = "rdx"
   stage               = "dev"
@@ -38,6 +90,10 @@ module "subnets" {
 }
 
 module "security_group" {
+  depends_on = [
+   null_resource.ecr_image
+  ]
+
   source = "terraform-aws-modules/security-group/aws//modules/http-80"
   name = "prediction-api-sg"
   vpc_id = data.aws_vpc.default.id
@@ -47,6 +103,10 @@ module "security_group" {
 
 
 module "alb" {
+   depends_on = [
+   null_resource.ecr_image
+  ]
+
   source = "terraform-aws-modules/alb/aws"
   
   version = "~> 5.0"
@@ -78,14 +138,93 @@ module "alb" {
 }
 
 resource "aws_ecs_cluster" "cluster" {
+  depends_on = [
+    null_resource.ecr_image,
+    data.aws_vpc.default,
+    data.aws_internet_gateway.default,
+    module.subnets,
+    module.security_group,
+    module.alb
+  ]
+
   name = "prediction-api-cluster"
 }
 
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs_task_execution_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
+         "Service": "ecs-tasks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "ecs_permissions" {
+  name        = "my_ecs_permissions"
+  description = "Permissions to enable CT"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Action": [
+        "ecs:CreateCluster",
+        "ecs:DeregisterContainerInstance",
+        "ecs:DiscoverPollEndpoint",
+        "ecs:Poll",
+        "ecs:RegisterContainerInstance",
+        "ecs:StartTelemetrySession",
+        "ecs:Submit*",
+        "ecs:StartTask",
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecs_attachment" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = aws_iam_policy.ecs_permissions.arn
+}
+
 module "container_definition" {
+  depends_on = [
+   null_resource.ecr_image,
+   data.aws_vpc.default,
+   data.aws_internet_gateway.default,
+   module.subnets,
+   module.security_group,
+   module.alb
+  ]
+
   source = "git::https://github.com/cloudposse/terraform-aws-ecs-container-definition.git?ref=tags/0.44.0"
 
   container_name  = "prediction-api-container"
-  container_image = "391350762509.dkr.ecr.us-west-2.amazonaws.com/prediction-api-repository"
+  container_image = "${aws_ecr_repository.api_ecr.repository_url}:${local.ecr_image_tag}"
+
   port_mappings   = [
     {
       containerPort = 80
@@ -96,6 +235,15 @@ module "container_definition" {
 }
 
 module "ecs_alb_service_task" {
+  depends_on = [
+    null_resource.ecr_image,
+    data.aws_vpc.default,
+    data.aws_internet_gateway.default,
+    module.subnets,
+    module.security_group,
+    module.alb
+  ]
+
   source = "git::https://github.com/cloudposse/terraform-aws-ecs-alb-service-task.git?ref=tags/0.40.2"
   
   namespace = "rdx"
@@ -109,12 +257,13 @@ module "ecs_alb_service_task" {
   subnet_ids = module.subnets.public_subnet_ids
   health_check_grace_period_seconds = 60
   ignore_changes_task_definition = false
+  assign_public_ip = true  
 
   ecs_load_balancers = [
     {
       target_group_arn = module.alb.target_group_arns[0]
       elb_name = ""
-      container_name = "prediction-api-container"
+      container_name = "prediction-api-container" 
       container_port = 80
     }
   ]
